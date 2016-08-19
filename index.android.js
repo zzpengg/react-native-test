530d437358cd60b5fb6e10f5ee6b4fb9fde8f2f5
yanhsu <e31217@gmail.com>
2016-08-19 16:48:34 +0800

    update

--- index.android.js
+++ index.android.js
@@ -9,31 +9,85 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  TextInput,
+  TouchableOpacity,
+  View,
+  Image
 } from 'react-native';
 
 class test extends Component {
+   constructor(props)
+   {
+     super(props);
+     this.state={
+       height:0,
+       weight:0,
+       bmi:0,
+       result:"",
+       page:'',
+       active:false
+     }
+   }
+    onPress = () => {
+    console.log('pressed');
+    var bmi=this.state.weight/(this.state.height/100*this.state.height/100);
+    var result="";
+    var active=!this.state.active;
+    if(bmi >25)
+       result="肥宅";
+    else
+       result="正常";
+    this.setState({result:result});
+    this.setState({bmi:bmi});
+    this.setState({active:active});
+    }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={[styles.container,this.state.active && styles.button]}>
+         <Image
+           style={styles.logo}
+           source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+         />
+         <Text>身高</Text>
+         <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(height) => this.setState({height})}
+          value={`${this.state.height}`}
+        />
+        <Text>體重</Text>
+         <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(weight) => this.setState({weight})}
+          value={`${this.state.weight}`}
+        />
+        <TouchableOpacity onPress={this.onPress}>
+          <Text>button</Text>
+        </TouchableOpacity>
+        <Text>{this.state.bmi}</Text>
+        <Text>狀態</Text>
+        <Text>{this.state.result}</Text>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(page) => this.setState({page})}
+          value={`${this.state.page}`}
+        />
+        <Image
+           style={styles.logo}
+           source={{uri: `https://pokeadvisor.com/img/mon/${this.state.page}.png`}}
+         />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  button:
+  {
+    backgroundColor:'#ff5533'
+  },
   container: {
     flex: 1,
+    flexDirection:'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
@@ -44,10 +98,17 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   instructions: {
+    flex:1,
     textAlign: 'center',
-    color: '#333333',
+    color: '#ff5533',
     marginBottom: 5,
   },
+  logo:{
+    justifyContent: 'center',
+    flex:1,
+    height: 70,
+    width: 90 
+  }
 });
 
 AppRegistry.registerComponent('test', () => test);
